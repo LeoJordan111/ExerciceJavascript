2b0e53c8d3b32f56da2ffbe54b54495bc55bedf3
Traur111 <leojordan.111@gmail.com>
2025-01-17 13:50:52 +0100

    Exercice0.3

---
+++ Exercice0.3/script.js
@@ -0,0 +1,17 @@
+
+// Exercice 0.3
+
+//Création de 3 variables
+    let genre = 'male'
+    let height = 182
+    let age = 35
+
+
+//Création d'un if pour comparer le genre
+if (genre === 'male') {
+    console.log("J'ai " + age + " ans, je mesure " + height + " centimètres et je suis un homme.");
+} else if (genre === 'female') {
+    console.log("J'ai " + age + " ans, je mesure " + height + " centimètres et je suis une femme.");
+} else {
+    console.log("Le genre n'est pas renseigné");
+}
