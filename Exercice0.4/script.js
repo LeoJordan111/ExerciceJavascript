4fd98771eb4273fadae2cba24f0310106bd88150
Traur111 <leojordan.111@gmail.com>
2025-01-17 13:59:31 +0100

    Exercice0.4

---
+++ Exercice0.4/script.js
@@ -0,0 +1,14 @@
+
+// Exercice 0.4
+
+//Création de 2 variables
+    let height = 182
+    let age = 35
+
+
+//Création d'un if pour comparer la taille et l'age
+if (height > 140 && age > 18) {
+    console.log("Je peux rentrer dans le manège");
+} else {
+    console.log("Je n'ai pas le droit de rentrer dans le manège");
+}
