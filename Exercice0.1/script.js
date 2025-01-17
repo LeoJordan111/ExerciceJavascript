1d905443cfe851bc5bdfd2ed8d71d500eecd0208
Traur111 <leojordan.111@gmail.com>
2025-01-17 12:28:41 +0100

    Exercice0.1

---
+++ Exercice0.1/script.js
@@ -0,0 +1,18 @@
+
+// Exercice 0.1
+
+//Création de 2 variables
+let nombre1 = 5
+let nombre2 = 6
+
+//Création d'un if pour comparer les 2 nombres
+// Si nombre1 = nombre2 --> j'affiche "Ce sont les mêmes"
+if (nombre1 === nombre2) {
+    console.log("Ce sont les mêmes !!");
+// Si nombre1 > nombre2 --> j'affiche "nombre1 > nombre2"
+} else if (nombre1 > nombre2) {
+    console.log(nombre1 + " > " + nombre2);
+// tout les autres cas soit nombre1 < nombre2 --> j'affiche "nombre1 < nombre2"
+} else {
+    console.log(nombre1 + " < " + nombre2);
+}
