75ca13542acfad28ff1ebf449a4594be7d5fd02f
Traur111 <leojordan.111@gmail.com>
2025-01-17 14:56:49 +0100

    Exercice2

---
+++ Exercice2/script.js
@@ -0,0 +1,8 @@
+
+// Exercice 2
+
+    //Création d'une fonction
+    function prix(prixHTproduit, nbreproduit) {
+        return (prixHTproduit + (prixHTproduit * 0.2)) * nbreproduit
+    }
+    console.log(prix(5, 3))
