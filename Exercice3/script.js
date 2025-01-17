c0e066ca5375737a9f3fdd2e9cfef27e4e09c046
Traur111 <leojordan.111@gmail.com>
2025-01-17 15:03:35 +0100

    Exercice3

---
+++ Exercice3/script.js
@@ -0,0 +1,12 @@
+
+// Exercice 3
+
+    // Etat de l'eau selon sa température
+    let temperature = 50
+    if (temperature < 0) {
+        console.log("Solide");
+    } else if (temperature > 100) {
+        console.log("gaz");
+    } else {
+        console.log("liquide");
+    }
