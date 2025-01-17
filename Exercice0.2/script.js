b5d7bb8debabde4dbe0c18f12e3ab0e896355eff
Traur111 <leojordan.111@gmail.com>
2025-01-17 13:38:24 +0100

    Exercice0.2

---
+++ Exercice0.2/script.js
@@ -0,0 +1,15 @@
+
+// Exercice 0.2
+
+//Création de la variable genre
+let genre = 'male'
+
+
+//Création d'un if pour comparer le genre
+if (genre === 'male') {
+    console.log("Je suis un homme");
+} else if (genre === 'female') {
+    console.log("Je suis une femme");
+} else {
+    console.log("Le genre n'est pas renseigné");
+}
