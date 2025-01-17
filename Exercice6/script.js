1c2db181930d58cb3c584c8679bb165e30a09289
Traur111 <leojordan.111@gmail.com>
2025-01-17 15:53:05 +0100

    Exercice6

---
+++ Exercice6/script.js
@@ -0,0 +1,10 @@
+
+// Exercice 6
+
+    let mavaleur = 2
+
+    // boucle pour enchainer la table de multiplication
+    for (i=1; i<13; i++) {
+        total = mavaleur * i
+        console.log("1*" + mavaleur + "=" + total)
+    }
